cb7a31ac1e4d75d537d1105dafd8225c6b2fff44
wai4043 <40437520@live.napier.ac.uk>
2020-01-23 09:29:59 +0630

    Capital city adding override methods

---
+++ src/main/java/com/napier/coursework/CapitalCity.java
@@ -0,0 +1,56 @@
+package com.napier.coursework;
+
+public class CapitalCity {
+    private String Name;
+
+    /**
+     * Capital City Name
+     */
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+    private String Country;
+
+
+
+    public String getCountry() {
+        return Country;
+    }
+    /**
+     * Capital City's country
+     */
+
+    public void setCountry(String country) {
+        Country = country;
+    }
+
+
+    private int Population;
+
+    /**
+     * Capital City's population
+     */
+    public int getPopulation() {
+        return Population;
+    }
+
+    @Override
+    public String toString() {
+        return "CapitalCity{" +
+                "Name='" + Name + '\'' +
+                ", Country='" + Country + '\'' +
+                ", Population=" + Population +
+                '}';
+    }
+
+    public void setPopulation(int population) {
+        Population = population;
+    }
+
+
+
+}
