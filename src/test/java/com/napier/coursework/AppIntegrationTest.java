4bcf8820d81582362c62423f6802e9e2bb8cbb1e
zweminthu <40437535@live.napier.ac.uk>
2020-01-31 15:42:33 +0630

    Add comment for integration testing

--- src/test/java/com/napier/coursework/AppIntegrationTest.java
+++ src/test/java/com/napier/coursework/AppIntegrationTest.java
@@ -2,19 +2,22 @@ package com.napier.coursework;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-
-import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * AppIntegrationTest Class for Integration Testing
+ */
 public class AppIntegrationTest {
+    /**
+     * Define static App
+     */
     static App app;
 
     @BeforeAll
     static void init()
-    {
+    {   /**
+     * Define new Object App
+     */
         app = new App();
         app.connect("localhost:33060");
     }
